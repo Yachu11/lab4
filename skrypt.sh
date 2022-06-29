f10d25a38d8e561454ad3f63a54109a1936fb158
Jan Ozga <jan.ozga111@gmail.com>
2022-06-29 22:00:11 +0200

    add logs

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,12 @@ if [ $1 = "--date" ]; then
     date=$(date)
     echo $date
 fi
+
+if [ $1 = "--logs" ]; then
+    for i in {1..100}
+    do
+        date=$(date)
+        name="log$i.txt"
+        echo "$name $0 $date" >> $name
+    done
+fi
