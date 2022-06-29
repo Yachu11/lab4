b86641d85758e6a1669c7724d8317f1e8b19c303
Jan Ozga <jan.ozga111@gmail.com>
2022-06-29 22:30:01 +0200

    new flags

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,11 @@
 flag=$1
 
-if [ $1 = "--date" ]; then
+if [ $1 = "--date" ] || [ $1 = "-d" ]; then
     date=$(date)
     echo $date
 fi
 
-if [ $1 = "--logs" ]; then
+if [ $1 = "--logs" ] || [ $1 = "-l" ]; then
     if [ $2 ]; then 
         for (( i=1; i<=$2; i++ ))
         do
@@ -23,10 +23,10 @@ if [ $1 = "--logs" ]; then
     fi
 fi
 
-if [ $1 = "--help" ]; then
+if [ $1 = "--help" ] || [ $1 = "-h" ]; then
     echo "
 - „skrypt.sh --date” wyświetli dzisiejszą datę
 - „skrypt.sh --logs” utworzy automatycznie 100 plików logx.txt
 - „skrypt.sh --logs 30” utworzenie automatycznie 30 plików, analogicznie
 jak wyżej, ale z obsługą argumentu liczby plików"
-fi
+fi 
